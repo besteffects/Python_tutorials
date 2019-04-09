d1a35963e19275e05fc0fac46ce53f522fbe385a
Vitalii <besteffects@gmail.com>
2019-04-09 02:12:28 -0300

    Initial commit. Arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,93 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 2+3
+5
+>>> 6-2
+4
+>>> 2**5
+32
+>>> 4/2
+2.0
+>>> 5/2
+2.5
+>>> 2/
+SyntaxError: invalid syntax
+>>> 2/3
+0.6666666666666666
+>>> 5/3
+1.6666666666666667
+>>> 7/3
+2.3333333333333335
+>>> 4//2
+2
+>>> 2//3
+0
+>>> 2/3
+0.6666666666666666
+>>> 5//3
+1
+>>> 7//3
+2
+>>> 4%2
+0
+>>> 2%3
+2
+>>> 5%3
+2
+>>> 7%3
+1
+>>> 3+4-5
+2
+>>> 4+5*3
+19
+>>> -10*3+5**3
+95
+>>> (4+5)*3
+27
+>>> -10*(3+5)**3
+-5120
+>>> 2+3
+5
+>>> (4+5*3
+
+ )
+19
+>>> 2+3
+5
+>>> 4/0
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    4/0
+ZeroDivisionError: division by zero
+>>> 20*12/2
+120.0
+>>> base=20
+>>> height=12
+>>> base
+20
+
+>>> height
+12
+>>> base*height/2
+120.0
+>>> area=base*height/2
+>>> area
+120.0
+>>> base=2.5
+>>> height=7
+>>> area=base*height/2
+>>> area
+8.75
+>>> 2.5*7/2
+8.75
+>>> seconda_in_minute=60
+>>> seConds_IN_Minute
+Traceback (most recent call last):
+  File "<pyshell#43>", line 1, in <module>
+    seConds_IN_Minute
+NameError: name 'seConds_IN_Minute' is not defined
+>>> seconda_in_minute
+60
+>>> hour's=7
+SyntaxError: EOL while scanning string literal
+>>> 
