860d08a66a12778e99053cc8dc34f691a23006eb
Vitalii <besteffects@gmail.com>
2019-04-25 02:06:25 -0300

    Added bool examples and stack example

---
+++ week3_bool_examples..py
@@ -0,0 +1,66 @@
+Python 3.4.3 (v3.4.3:9b73f1c3e601, Feb 24 2015, 22:44:40) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 3<4
+True
+>>> 3>5
+False
+>>> 8>3
+True
+>>> 3.5 >3.4
+True
+>>> 7==7
+True
+>>> 7==7.0
+True
+>>> x=7
+>>> y=8
+>>> x==y
+False
+>>> x==7
+True
+>>> 3!=4
+True
+>>> grade=80
+>>> grade>=50
+True
+>>> not(grade>=50)
+False
+>>> grade2 =70
+>>> (grade<=50) and (grade2 >=50)
+False
+>>> grade>=50
+True
+>>> grade2=70
+>>> (grade<=50) and (grade2 >=50)
+False
+>>> (grade>=50) and (grade2 >=50)
+True
+>>> grade =40
+>>> (grade>=50) and (grade2 >=50)
+False
+>>> grade =80
+>>> grade2=40
+>>> (grade>=50) and (grade2 >=50)
+False
+>>> grade =70
+>>> grade2 =80
+>>> (grade >=50) or (grade2 >= 50)
+True
+>>> grade =40
+>>> (grade >=50) or (grade2 >= 50)
+True
+>>> grade2=40
+>>> grade=80
+>>> (grade >=50) or (grade2 >= 50)
+True
+>>> grade=80
+>>> grade2=90
+>>> not grade >=50 or grade2 >= 50
+True
+>>> not (grade >=50 or grade2 >= 50)
+False
+>>> (not grade >=50) or grade2 >= 50
+True
+>>> (4!=4) or 2>3
+False
+>>> 
